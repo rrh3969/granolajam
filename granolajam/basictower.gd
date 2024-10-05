081ee92fc3286dcf661b8251dc883729118ab813
rrh3969 <rrh3969@rit.edu>
2024-10-04 23:24:26 -0400

    jdksfjjfkdljf

--- granolajam/basictower.gd
+++ granolajam/basictower.gd
@@ -1,18 +1,17 @@
-extends Node2D
+extends CharacterBody2D
 
 var enemyInRange = false
 var enemy 
 
 func _physics_process(delta: float) -> void:
-	if(enemyInRange):
-		rotation = enemy.position.angle_to_point(position)
-	else:
+	#look_at(get_global_mouse_position())
+	if(enemy):
+		look_at(enemy.position)
 		pass
 
 func _on_range_body_entered(body: Node2D) -> void:
-	enemyInRange = true
 	enemy = body
 
 
 func _on_range_body_exited(body: Node2D) -> void:
-	enemyInRange = false
+	enemy = null
